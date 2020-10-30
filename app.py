b68fda0b13ba0bb69b687f3234936f95f8982a9a
Damian Günzing <65827185+gnzng@users.noreply.github.com>
2020-10-30 23:58:54 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,230 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import pandas as pd
+import plotly.express as px
+import os
+import numpy as np
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+def zahl2land(b):
+    c = "{0:02d}".format(b)
+    a = str(c)
+    zahl2landdic = {'00': 'Deutschland',
+    '01': 'Schleswig-Holstein',
+    '02': 'Freie und Hansestadt Hamburg',
+    '03': 'Niedersachsen',
+    '04': 'Freie Hansestadt Bremen',
+    '05': 'Nordrhein-Westfalen',
+    '06': 'Hessen',
+    '07': 'Rheinland-Pfalz',
+    '08': 'Baden-Württemberg',
+    '09': 'Freistaat Bayern',
+    '10': 'Saarland',
+    '11': 'Berlin',
+    '12': 'Brandenburg',
+    '13': 'Mecklenburg-Vorpommern',
+    '14': 'Freistaat Sachsen',
+    '15': 'Sachsen-Anhalt',
+    '16': 'Freistaat Thüringen'}
+    land2zahldic = {v: k for k, v in zahl2landdic.items()}
+    return zahl2landdic[a]
+
+def importcsv(a):
+    return pd.read_csv(a, parse_dates=['daten_stand'])
+betten_data = os.listdir('betten_data')
+
+bettenlst = []
+for n in sorted(betten_data):
+    if n.endswith('.csv'):
+        bettenlst.append(importcsv('betten_data/{}'.format(n)))
+
+df = pd.concat(bettenlst)
+
+
+
+def give_cases(a,df):
+    if a == 0:
+        return df.groupby(['daten_stand']).sum().reset_index()
+    if a < 17:
+        df = df[df['bundesland'] == a].groupby(['daten_stand']).sum().reset_index()
+        return df
+    if a > 17:
+        return df[df['gemeindeschluessel'] == a]
+
+
+available_indicators = np.insert(df['bundesland'].unique(),0,0)
+print(available_indicators)
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app.layout = html.Div([
+    dcc.Dropdown(
+        id='demo-dropdown',
+        options=[{'label': zahl2land(i)+': Intesivstationssituation', 'value': i} for i in available_indicators],
+        value=0
+    ),
+    dcc.Graph(
+        id='my-graph'
+    )
+])
+
+
+@app.callback(
+    dash.dependencies.Output('my-graph', 'figure'),
+    [dash.dependencies.Input('demo-dropdown', 'value')])
+#def update_output(value):
+#    return '{}'.format(zahl2land(value))
+        #[zahl2land(n) for n in value])
+def update_graph(value):
+    dff =  give_cases(value,df)
+    return {
+        'data': [
+            {'x': dff['daten_stand'],'y': dff['betten_frei'],'name': 'Betten frei'},
+            {'x': dff['daten_stand'],'y': dff['betten_belegt'],'name': 'Betten belegt'},
+            {'x': dff['daten_stand'],'y': dff['faelle_covid_aktuell'],'name': 'stationär wg. COVID'},
+            {'x': dff['daten_stand'],'y': dff['faelle_covid_aktuell_beatmet'],'name': 'beatmet wg. COVID'}
+
+        ],
+        'layout': {'margin': {'l': 40, 'r': 0, 't': 20, 'b': 30}},
+    }
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
+#######
+'''
+
+app.layout = html.Div([
+    html.Div([
+        html.Div([
+            dcc.Dropdown(
+                id='crossfilter-xaxis-column',
+                options=[{'label': i, 'value': i} for i in available_indicators],
+                value='0'
+            ),
+            dcc.RadioItems(
+                id='crossfilter-xaxis-type',
+                options=[{'label': i, 'value': i} for i in ['Linear', 'Log']],
+                value='Linear',
+                labelStyle={'display': 'inline-block'}
+            )
+        ],
+        style={'width': '49%', 'display': 'inline-block'}),
+
+        html.Div([
+            dcc.Dropdown(
+                id='crossfilter-yaxis-column',
+                options=[{'label': i, 'value': i} for i in available_indicators],
+                value='Life expectancy at birth, total (years)'
+            ),
+            dcc.RadioItems(
+                id='crossfilter-yaxis-type',
+                options=[{'label': i, 'value': i} for i in ['Linear', 'Log']],
+                value='Linear',
+                labelStyle={'display': 'inline-block'}
+            )
+        ], style={'width': '49%', 'float': 'right', 'display': 'inline-block'})
+    ], style={
+        'borderBottom': 'thin lightgrey solid',
+        'backgroundColor': 'rgb(250, 250, 250)',
+        'padding': '10px 5px'
+    }),
+
+    html.Div([
+        dcc.Graph(
+            id='crossfilter-indicator-scatter',
+            hoverData={'points': [{'customdata': 'Japan'}]}
+        )
+    ], style={'width': '49%', 'display': 'inline-block', 'padding': '0 20'}),
+    html.Div([
+        dcc.Graph(id='x-time-series'),
+        dcc.Graph(id='y-time-series'),
+    ], style={'display': 'inline-block', 'width': '49%'}),
+
+    html.Div(dcc.Slider(
+        id='crossfilter-year--slider',
+        min=df['daten_stand'].min(),
+        max=df['daten_stand'].max(),
+        value=df['Year'].max(),
+        marks={str(year): str(year) for year in df['Year'].unique()},
+        step=None
+    ), style={'width': '49%', 'padding': '0px 20px 20px 20px'})
+])
+
+
+@app.callback(
+    dash.dependencies.Output('crossfilter-indicator-scatter', 'figure'),
+    [dash.dependencies.Input('crossfilter-xaxis-column', 'value'),
+     dash.dependencies.Input('crossfilter-yaxis-column', 'value'),
+     dash.dependencies.Input('crossfilter-xaxis-type', 'value'),
+     dash.dependencies.Input('crossfilter-yaxis-type', 'value'),
+     dash.dependencies.Input('crossfilter-year--slider', 'value')])
+def update_graph(xaxis_column_name, yaxis_column_name,
+                 xaxis_type, yaxis_type,
+                 year_value):
+    dff = df[df['Year'] == year_value]
+
+    fig = px.scatter(x=dff[dff['Indicator Name'] == xaxis_column_name]['Value'],
+            y=dff[dff['Indicator Name'] == yaxis_column_name]['Value'],
+            hover_name=dff[dff['Indicator Name'] == yaxis_column_name]['Country Name']
+            )
+
+    fig.update_traces(customdata=dff[dff['Indicator Name'] == yaxis_column_name]['Country Name'])
+
+    fig.update_xaxes(title=xaxis_column_name, type='linear' if xaxis_type == 'Linear' else 'log')
+
+    fig.update_yaxes(title=yaxis_column_name, type='linear' if yaxis_type == 'Linear' else 'log')
+
+    fig.update_layout(margin={'l': 40, 'b': 40, 't': 10, 'r': 0}, hovermode='closest')
+
+    return fig
+
+
+def create_time_series(dff, axis_type, title):
+
+    fig = px.scatter(dff, x='Year', y='Value')
+
+    fig.update_traces(mode='lines+markers')
+
+    fig.update_xaxes(showgrid=False)
+
+    fig.update_yaxes(type='linear' if axis_type == 'Linear' else 'log')
+
+    fig.add_annotation(x=0, y=0.85, xanchor='left', yanchor='bottom',
+                       xref='paper', yref='paper', showarrow=False, align='left',
+                       bgcolor='rgba(255, 255, 255, 0.5)', text=title)
+
+    fig.update_layout(height=225, margin={'l': 20, 'b': 30, 'r': 10, 't': 10})
+
+    return fig
+
+
+@app.callback(
+    dash.dependencies.Output('x-time-series', 'figure'),
+    [dash.dependencies.Input('crossfilter-indicator-scatter', 'hoverData'),
+     dash.dependencies.Input('crossfilter-xaxis-column', 'value'),
+     dash.dependencies.Input('crossfilter-xaxis-type', 'value')])
+def update_y_timeseries(hoverData, xaxis_column_name, axis_type):
+    country_name = hoverData['points'][0]['customdata']
+    dff = df[df['Country Name'] == country_name]
+    dff = dff[dff['Indicator Name'] == xaxis_column_name]
+    title = '<b>{}</b><br>{}'.format(country_name, xaxis_column_name)
+    return create_time_series(dff, axis_type, title)
+
+
+@app.callback(
+    dash.dependencies.Output('y-time-series', 'figure'),
+    [dash.dependencies.Input('crossfilter-indicator-scatter', 'hoverData'),
+     dash.dependencies.Input('crossfilter-yaxis-column', 'value'),
+     dash.dependencies.Input('crossfilter-yaxis-type', 'value')])
+def update_x_timeseries(hoverData, yaxis_column_name, axis_type):
+    dff = df[df['Country Name'] == hoverData['points'][0]['customdata']]
+    dff = dff[dff['Indicator Name'] == yaxis_column_name]
+    return create_time_series(dff, axis_type, yaxis_column_name)
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)'''
